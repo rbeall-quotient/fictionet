5773523a149992c9b5659c30f113627875f318bf
Robert Beall <rbeall@quotient-inc.com>
2022-04-05 09:37:19 -0400

    Added story models

--- fictionet/magazine/models.py
+++ fictionet/magazine/models.py
@@ -15,11 +15,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from taggit.managers import TaggableManager
 
+
 # Create your models here.
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
     extension = os.path.splitext(filename)[1]
     return 'users/user_{0}/{1}'.format(instance.user.id, 'profile' + extension)
+
 
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -35,3 +37,36 @@ class profile(models.Model):
         if created:
             profile.objects.create(user=instance)
         instance.profile.save()
+
+
+class Submission(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = RichTextField()
+    tags = TaggableManager()
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Story(Submission):
+    subtitle = models.CharField(max_length=200)
+    ratings = GenericRelation(Rating, related_query_name='storys')
+
+    class Genre(models.TextChoices):
+        LIT_FIC = 'LF', _('Literary Fiction')
+        SCI_FI = 'SF', _('Science Fiction')
+        FAN = 'F', _('Fantasy')
+        HRR = 'HR', _('Horror')
+        MYS = 'MYS', _('Mystery')
+        HIS = 'HIS', _('Historical Fiction')
+        ROM = 'ROM', _('Romance')
+
+    genre = models.CharField(
+        max_length=3,
+        choices=Genre.choices,
+        default=Genre.LIT_FIC,
+    )
